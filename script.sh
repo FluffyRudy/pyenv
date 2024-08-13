6ad661522674226ae9e98ba50db49912fafb070f
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-08-13 16:38:50 +0545

    Update - Add `/usr/local/bin` to search - Omit listing non executable/config python related file

--- script.sh
+++ script.sh
@@ -6,11 +6,30 @@ pyactivate() {
 
   if [ ! -d "$1" ]; then
     echo "Virtual environment not found. Creating a new one."
-    
+
     echo "Available Python interpreters:"
-    py_versions=($(ls /usr/bin/python* | grep -E 'python[0-9]+\.[0-9]+'))
+    py_dirs=("/usr/bin" "/usr/local/bin" "$HOME/.local/bin")
+    py_versions=()
+    py_version_display=()
+
+    for dir in "${py_dirs[@]}"; do
+      if [ -d "$dir" ]; then
+        files=($(find "$dir" -maxdepth 1 -type f -executable -name 'python*' ! -name '*-coverage' ! -name '*-config' 2>/dev/null))
+        for file in "${files[@]}"; do
+          version_output=$("$file" --version 2>&1)
+          if [[ $version_output =~ ([0-9]+\.[0-9]+\.[0-9]+) ]]; then
+            py_versions+=("$file")
+            py_version_display+=("${file}: ${BASH_REMATCH[1]}")
+          else
+            py_versions+=("$file")
+            py_version_display+=("$(basename "$file")")
+          fi
+        done
+      fi
+    done
+
     for i in "${!py_versions[@]}"; do
-      echo "$i) ${py_versions[$i]}"
+      echo "$i) ${py_version_display[$i]} (${py_versions[$i]})"
     done
 
     read -p "Select Python interpreter (default is system-wide Python 3): " choice
